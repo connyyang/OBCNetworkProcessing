c798c23fcaebe86bb6f99ee08f299d46ab9d938e
Conny Yang <connyyang097@gmail.com>
2017-02-04 23:19:52 +1100

    Use Objective-c to connect Rest API

---
+++ OBCNetworkProcessing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OBCNetworkProcessing
+//
+//  Created by Conny Yang on 3/02/2017.
+//  Copyright © 2017 Conny Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBCNetworkProcessing/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  OBCNetworkProcessing
+//
+//  Created by Conny Yang on 3/02/2017.
+//  Copyright © 2017 Conny Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NetworkProcessing.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
